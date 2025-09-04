a2e07d23740d428bcdd41258a40c684be893b9e5
tanishenigma <tanish23enigma@gmail.com>
2025-09-04 23:52:30 +0530

    create-link

--- Frontend/src/components/CreateLink.jsx
+++ Frontend/src/components/CreateLink.jsx
@@ -1,10 +1,118 @@
+import { Filter, X } from "lucide-react";
 import React from "react";
+import LinkCard from "./LinkCard";
 
 // const handleCreate = async () => {
 //   const { data, error } = await supabase.from("urls").insert([{}]);
 // };
-const CreateLink = () => {
-  return <div>CreateLink</div>;
+const CreateLink = ({
+  create,
+  setCreate,
+  filteredUrls,
+  setSearchQuery,
+  setUrls,
+  urls,
+  clicks,
+}) => {
+  return (
+    <div className="relative flex justify-center items-start w-full ">
+      <div
+        className={
+          create
+            ? `fixed mt-10 bg-primary/10 backdrop-blur-sm flex flex-col items-center gap-y-5 z-200 rounded-xl p-15 py-25 drop-shadow-2xl transition-all duration-100`
+            : ` -top-200 bg-primary/10 backdrop-blur-sm flex flex-col items-center gap-y-5 z-200 rounded-xl p-15 py-25 drop-shadow-2xl transition-all duration-100`
+        }>
+        <X
+          className="cursor-pointer absolute bottom-168 left-98"
+          onClick={() => {
+            setCreate(!create);
+          }}
+          size={30}
+        />
+        <div className="flex justify-between w- ">
+          <h1 className="text-5xl font-black "> Create Lynk</h1>
+        </div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png"
+          className="size-50"
+        />
+        <input
+          placeholder="Name of your lynk"
+          className="bg-primary/10 w-full border-2 border-primary/10 p-2 rounded-xl outline-none"
+        />
+        <input
+          placeholder="Enter Your Lynk"
+          className="bg-primary/10 w-full border-2 border-primary/10 p-2 rounded-xl outline-none"
+        />
+        <div className="bg-primary/10 w-fit border-2 border-primary/10 p-2 rounded-xl o">
+          <span>Enc.ly/ </span>
+          <input
+            className="outline-none"
+            placeholder="Custom Lynk (optional)"
+          />
+        </div>
+
+        <button className="bg-primary p-2 w-full rounded-full cursor-pointer text-slate-50">
+          Encly
+        </button>
+      </div>
+
+      <div className="w-full blur-md">
+        <div className="flex w-full gap-5 items-center mb-20">
+          <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-xl">
+            Links Created
+            <p className="pt-5 ">{urls?.length}</p>
+          </div>
+
+          <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-md ">
+            Total Clicks
+            <p className="pt-5">{clicks?.length}</p>
+          </div>
+        </div>
+        <div className="border-1 border-purple-400/20 ">
+          <div className="">
+            <div className="flex justify-between py-15">
+              <h1 className="text-6xl font-bold">My Links</h1>
+              <button className="bg-primary rounded-lg px-5">
+                Create Link
+              </button>
+            </div>
+            <div className="flex justify-end w-full bg-primary/10 rounded-md p-4 mt-5 border-2 border-primary/20 relative">
+              <input
+                className="w-full focus-within:outline-0"
+                placeholder="Filter Links..."
+                onChange={(e) => {
+                  setSearchQuery(e.target.value);
+                }}
+              />
+              <Filter className="absolute hover:scale-110 duration-300 cursor-pointer " />
+            </div>
+            <div>
+              <div className="flex bg-primary/10 w-full h-fit mt-5 rounded-xl p-5 ">
+                {urls.length != 0 ? (
+                  <div className="min-w-full border border-primary/30 text-slate-50 text-left  md:flex-row">
+                    <div className="bg-primary/10"></div>
+                    <div>
+                      {(filteredUrls || []).map((u, i) => (
+                        <LinkCard
+                          key={i}
+                          url={u}
+                          filteredUrls={filteredUrls}
+                          setUrls={setUrls}
+                        />
+                      ))}
+                    </div>
+                  </div>
+                ) : (
+                  <p className="text-slate-400 font-light"> No Links Yet</p>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CreateLink;
--- Frontend/src/pages/LinkPage.jsx
+++ Frontend/src/pages/LinkPage.jsx
@@ -5,9 +5,8 @@ import { getClicks } from "../services/clickServices";
 import { getUserUrls } from "../services/urlService";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../lib/firebase";
-import { Cross, CrossIcon, Filter, X } from "lucide-react";
+import { Filter } from "lucide-react";
 import LinkCard from "../components/LinkCard";
-import supabase from "../db/supabase";
 import CreateLink from "../components/CreateLink";
 
 const LinkPage = () => {
@@ -58,10 +57,10 @@ const LinkPage = () => {
 
   return (
     <>
-      {create ? (
+      {!create ? (
         <div className="w-full">
           <div className="flex w-full gap-5 items-center mb-20">
-            <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-md">
+            <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-xl">
               Links Created
               <p className="pt-5 ">{urls?.length}</p>
             </div>
@@ -119,81 +118,15 @@ const LinkPage = () => {
           </div>
         </div>
       ) : (
-        <div className="relative flex justify-center items-start w-full">
-          <div className="fixed mt-30 bg-primary/10 backdrop-blur-sm  flex flex-col items-center gap-y-5 z-200 rounded-xl p-15 py-25 drop-shadow-2xl">
-            <X
-              className="cursor-pointer absolute top-5 left-80"
-              onClick={() => {
-                setCreate(!create);
-              }}
-            />
-            <div className="flex justify-between gap-x-2 ">
-              <h1 className="text-5xl font-black "> Create link</h1>
-            </div>
-            <input
-              placeholder="Enter Your Lynk"
-              className="bg-primary/10 border-2 border-primary/10 p-2 rounded-xl outline-none"
-            />
-            <button className="bg-primary p-4 w-60 rounded-full cursor-pointer text-slate-50">
-              Encly
-            </button>
-          </div>
-
-          <div className="w-full blur-md">
-            <div className="flex w-full gap-5 items-center mb-20">
-              <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-xl">
-                Links Created
-                <p className="pt-5 ">{urls?.length}</p>
-              </div>
-
-              <div className="bg-primary/5 w-full text-2xl font-semibold  text-slate-50 rounded-xl p-10 h-50 border-2 border-primary/20 backdrop-blur-md ">
-                Total Clicks
-                <p className="pt-5">{clicks?.length}</p>
-              </div>
-            </div>
-            <div className="border-1 border-purple-400/20 ">
-              <div className="">
-                <div className="flex justify-between py-15">
-                  <h1 className="text-6xl font-bold">My Links</h1>
-                  <button className="bg-primary rounded-lg px-5">
-                    Create Link
-                  </button>
-                </div>
-                <div className="flex justify-end w-full bg-primary/10 rounded-md p-4 mt-5 border-2 border-primary/20 relative">
-                  <input
-                    className="w-full focus-within:outline-0"
-                    placeholder="Filter Links..."
-                    onChange={(e) => {
-                      setSearchQuery(e.target.value);
-                    }}
-                  />
-                  <Filter className="absolute hover:scale-110 duration-300 cursor-pointer " />
-                </div>
-                <div>
-                  <div className="flex bg-primary/10 w-full h-fit mt-5 rounded-xl p-5 ">
-                    {urls.length != 0 ? (
-                      <div className="min-w-full border border-primary/30 text-slate-50 text-left  md:flex-row">
-                        <div className="bg-primary/10"></div>
-                        <div>
-                          {(filteredUrls || []).map((u, i) => (
-                            <LinkCard
-                              key={i}
-                              url={u}
-                              filteredUrls={filteredUrls}
-                              setUrls={setUrls}
-                            />
-                          ))}
-                        </div>
-                      </div>
-                    ) : (
-                      <p className="text-slate-400 font-light"> No Links Yet</p>
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <CreateLink
+          filteredUrls={filteredUrls}
+          create={create}
+          setCreate={setCreate}
+          setSearchQuery={setSearchQuery}
+          setUrls={setUrls}
+          urls={urls}
+          clicks={clicks}
+        />
       )}
     </>
   );
