b208dfbc068f796235680897bff87bc2ec2d680d
tanishenigma <tanish23enigma@gmail.com>
2025-09-06 03:18:43 +0530

    responsive linkPage

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <div className="no-scrollbar">
+    <div>
       <App />
     </div>
   </StrictMode>
